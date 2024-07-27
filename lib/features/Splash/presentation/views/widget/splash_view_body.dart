d7bcef535bdfa9b540a27f7fdc184c27601ec410
mohamed <mohamdsalah2025@gmail.com>
2024-07-27 13:20:21 +0200

    commit

---
+++ lib/features/Splash/presentation/views/widget/splash_view_body.dart
@@ -0,0 +1,49 @@
+import 'package:ecommerce_app/features/Auth/presentation/views/forget_password_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 4), () {
+      Get.to(
+        () => const ForgotPasswordView(),
+        transition: Transition.rightToLeft,
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/Screenshot (773) 1.png'),
+          const SizedBox(height: 10),
+          SvgPicture.asset(
+            'assets/images/STYLISH Find Your Slyle.svg',
+          )
+              .animate()
+              .scale(
+                duration: const Duration(seconds: 3),
+              )
+              .then()
+              .shake(
+                hz: 3,
+              ),
+        ],
+      ),
+    );
+  }
+}
