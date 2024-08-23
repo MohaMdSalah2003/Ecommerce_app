af86b304b679ccf9e3797a1f3e3841a6f014e749
mohamed <mohamdsalah2025@gmail.com>
2024-08-23 15:59:12 +0200

    small_changes

--- lib/features/Home/presentation/view/widget/custom_brand_item.dart
+++ lib/features/Home/presentation/view/widget/custom_brand_item.dart
@@ -11,7 +11,7 @@ class CustomBrandItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 115,
+      // width: 115,
       height: 50,
       decoration: ShapeDecoration(
         color: const Color(0xFFF5F6FA),
